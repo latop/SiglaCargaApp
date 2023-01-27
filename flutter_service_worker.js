ac9621201fd761df85f6c8dc84d5fc0615f5fc66
HFCristian <cristianhassfretes@gmal.com>
2023-01-27 15:46:33 -0400

    update 27/01

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,10 +6,10 @@ const RESOURCES = {
   "version.json": "97eb7e5a4accf029f26c1f230aa3a9ee",
 "splash/splash.js": "c6a271349a0cd249bdb6d3c4d12f5dcf",
 "splash/style.css": "cf46f7f07ad5ec9ea98393b4bd4aa625",
-"index.html": "da9a1af458d446231e5b8691ec75a9ea",
-"/": "da9a1af458d446231e5b8691ec75a9ea",
+"index.html": "46a8efabea10c3eb94e0344b488a6853",
+"/": "46a8efabea10c3eb94e0344b488a6853",
 "main.dart.js": "49c10b381a0b5783756caa6b02ba2c10",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -54,10 +54,10 @@ const RESOURCES = {
 "assets/assets/icons/Documents.svg": "51896b51d35e28711cf4bd218bde185d",
 "assets/assets/icons/excle_file.svg": "dc91b258ecf87f5731fb2ab9ae15a3ec",
 "assets/assets/icons/one_drive.svg": "aa908c0a29eb795606799385cdfc8592",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
